f3cbd17492bf1a0bdba259ec3318eebac16e2449
Xiaoxing Hu <hi@xiaoxing.dev>
2023-10-08 19:12:26 +1300

    add fonts

--- darwin.nix
+++ darwin.nix
@@ -3,13 +3,19 @@
   services.nix-daemon.enable = true;
   nix.package = pkgs.nixFlakes;
 
-  # services.emacs = {
-  #   enable = true;
-  #   package = pkgs.emacs29-macport;
-  # };
+  fonts = {
+    enableFontDir = true;
+    fonts = with pkgs; [
+      jetbrains-mono
+      fira-code
+      meslo-lgs-nf
+      (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" ]; })
+    ];
+  };
 
   environment.systemPackages = with pkgs; [
     emacs29-macport
   ];
+
   programs.zsh.enable = true;
 }
